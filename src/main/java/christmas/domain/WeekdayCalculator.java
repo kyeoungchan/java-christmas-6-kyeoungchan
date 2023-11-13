9dc6fbaf64977e97c945a2ae5741e9d444a107de
kyeoungchan <wkc1002@naver.com>
2023-11-13 21:10:58 +0900

    feat: WeekdaysCalculator의 적용날짜 판별 기능 구현 완료

--- src/main/java/christmas/domain/WeekdayCalculator.java
+++ src/main/java/christmas/domain/WeekdayCalculator.java
@@ -1,2 +1,17 @@
-package christmas.domain;public class WeekdayCalculator {
+package christmas.domain;
+
+import christmas.vo.Day;
+import christmas.vo.Money;
+import christmas.vo.Order;
+
+public class WeekdayCalculator implements EventCalculatorAdapter {
+    @Override
+    public boolean supports(Day day, Money priceBeforeDiscount) {
+        return day.isWeekday();
+    }
+
+    @Override
+    public int discountPrice(Order order) {
+        return 0;
+    }
 }
