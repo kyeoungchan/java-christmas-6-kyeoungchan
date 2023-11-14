fc8c96b8ca60fb3b1e1a871f4b4c115dd11e6858
kyeoungchan <wkc1002@naver.com>
2023-11-14 15:54:06 +0900

    test: Presentation supports()

--- src/test/java/christmas/domain/PresentationCalculatorTest.java
+++ src/test/java/christmas/domain/PresentationCalculatorTest.java
@@ -1,4 +1,44 @@
-import static org.junit.jupiter.api.Assertions.*;
+package christmas.domain;
+
+import christmas.consts.ConstantMoney;
+import christmas.consts.Menu;
+import christmas.vo.Day;
+import christmas.vo.Money;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class PresentationCalculatorTest {
-  
+    private final PresentationCalculator presentationCalculator = new PresentationCalculator();
+
+    @Test
+    @DisplayName("PresentationCalculator는 총주문 가격이 12만원 이상이면 기능을 지원한다.")
+    void supports() {
+        int moneyIncreaseUnit = 100;
+        int tempLoopCount = 1000;
+        Day tempDay = new Day(1);
+        for (int i = 0; i < tempLoopCount; i++) {
+            int totalAmount = ConstantMoney.STANDARD_FOR_PRESENTATION_EVENT.getAmount()
+                    + moneyIncreaseUnit * i; // 12만원 + 100원 * i
+            assertThat(presentationCalculator.supports(tempDay, new Money(totalAmount)))
+                    .isTrue();
+        }
+    }
+
+    @Test
+    @DisplayName("PresentationCalculator는 총주문 가격이 12만원 이하면 기능을 지원하지 않는다.")
+    void notSupports() {
+        int minimumPrice = Menu.COKE_ZERO.getAmount(); // 메뉴 중에서 제로 콜라가 제일 싸다.
+        int moneyIncreaseUnit = 100;
+        int loopCount = (ConstantMoney.STANDARD_FOR_PRESENTATION_EVENT.getAmount() - minimumPrice)
+                / moneyIncreaseUnit; // (12만원 - 최소 주문 금액) / 100원
+        Day tempDay = new Day(1);
+        for (int i = 0; i < loopCount; i++) {
+            int totalAmount = minimumPrice + moneyIncreaseUnit * i; // 최소 주문 금액 + 100원 * i
+            assertThat(presentationCalculator.supports(tempDay, new Money(totalAmount)))
+                    .isFalse();
+        }
+
+    }
 }
