f4f058b7006d7bca11d8978a890ad2c6fa5366e2
kyeoungchan <wkc1002@naver.com>
2023-11-14 14:45:13 +0900

    test: WeekendCalculatorTest discountMainMenu function

--- src/test/java/christmas/domain/WeekendCalculatorTest.java
+++ src/test/java/christmas/domain/WeekendCalculatorTest.java
@@ -1,13 +1,17 @@
 package christmas.domain;
 
 import christmas.consts.ConstantDate;
+import christmas.consts.ConstantMoney;
+import christmas.consts.Menu;
 import christmas.vo.Day;
 import christmas.vo.Money;
+import christmas.vo.Order;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,15 +19,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 class WeekendCalculatorTest {
     private final WeekendCalculator weekendCalculator = new WeekendCalculator();
     private final List<Day> supportedDays = new ArrayList<>();
+    private final EnumMap<Menu, Integer> menuCount = new EnumMap<>(Menu.class);
 
     @BeforeEach
-    void initializeSupportedDays() {
+    void initializeSupportedDaysAndMenuesWithoutMain() {
         for (int date = 0; date <= ConstantDate.LAST_DATE.getDate(); date++) {
             Day day = new Day(date);
             if (day.isWeekend()) {
                 supportedDays.add(day);
             }
         }
+        menuCount.put(Menu.BUTTON_MUSHROOM_SOUP, 1);
+        menuCount.put(Menu.TAPAS, 2);
+        menuCount.put(Menu.CHOCOLATE_CAKE, 3);
+        menuCount.put(Menu.CHAMPAGNE, 2);
     }
 
     @Test
@@ -45,5 +54,26 @@ class WeekendCalculatorTest {
                 assertThat(weekendCalculator.supports(day, tempMoney)).isFalse();
             }
         }
+    }
+
+    @Test
+    @DisplayName("메인 메뉴 개수만큼 할인 가격이 결정된다.")
+    void discountPrice() {
+        int tBoneSteakCount = 5;
+        menuCount.put(Menu.T_BONE_STEAK, tBoneSteakCount);
+        int seafoodPastaCount = 3;
+        menuCount.put(Menu.SEAFOOD_PASTA, seafoodPastaCount);
+
+        Order order = new Order(new Day(1), menuCount);
+        assertThat(weekendCalculator.discountPrice(order))
+                .isEqualTo(-1 * ConstantMoney.INCREASE_UNIT_FOR_WEEKDAY_EVENT.getAmount()
+                        * (tBoneSteakCount + seafoodPastaCount));
+    }
+
+    @Test
+    @DisplayName("메인 메뉴가 없으면 할인 가격은 0원이다.")
+    void discountNothing() {
+        Order order = new Order(new Day(1), menuCount);
+        assertThat(weekendCalculator.discountPrice(order)).isEqualTo(0);
     }
 }
