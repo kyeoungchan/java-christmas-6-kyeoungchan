88cf0ba6d4aa7b819bf7fa1fc5aa27845fdd932a
kyeoungchan <wkc1002@naver.com>
2023-11-14 17:23:07 +0900

    test: PresentationCalculator discountPrice()

--- src/test/java/christmas/domain/PresentationCalculatorTest.java
+++ src/test/java/christmas/domain/PresentationCalculatorTest.java
@@ -2,11 +2,15 @@ package christmas.domain;
 
 import christmas.consts.ConstantMoney;
 import christmas.consts.Menu;
+import christmas.testconsts.TestConsts;
 import christmas.vo.Day;
 import christmas.vo.Money;
+import christmas.vo.Order;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static christmas.testconsts.TestConsts.tempDay;
+import static christmas.testconsts.TestConsts.tempMenuCounts;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PresentationCalculatorTest {
@@ -36,9 +40,36 @@ class PresentationCalculatorTest {
         Day tempDay = new Day(1);
         for (int i = 0; i < loopCount; i++) {
             int totalAmount = minimumPrice + moneyIncreaseUnit * i; // 최소 주문 금액 + 100원 * i
-            assertThat(presentationCalculator.supports(tempDay, new Money(totalAmount)))
-                    .isFalse();
+            assertThat(presentationCalculator.supports(tempDay, new Money(totalAmount))).isFalse();
         }
+    }
 
+    @Test
+    @DisplayName("증정 대상이라면 12만원에 한 개의 샴페인 가격만큼 혜택을 준다.")
+    void discountPrice() {
+        int moneyIncreaseUnit = 100;
+        int loopCount = ConstantMoney.STANDARD_FOR_PRESENTATION_EVENT.getAmount() / moneyIncreaseUnit;
+        for (int i = 0; i < loopCount; i++) {
+            Money totalOrderPrice = new Money( // 12만원 + (100원 * i)
+                    ConstantMoney.STANDARD_FOR_PRESENTATION_EVENT.getAmount() + moneyIncreaseUnit * i);
+            Order order = new Order(tempDay, tempMenuCounts, totalOrderPrice);
+            assertThat(presentationCalculator.discountPrice(order)).isEqualTo(
+                    ConstantMoney.SIGN_INVERTER.getAmount() * Menu.CHAMPAGNE.getAmount());
+        }
+    }
+
+    @Test
+    @DisplayName("12만원 배수만큼 주문하면 그 배수만큼의 샴페인 가격만큼 혜택을 준다.")
+    void discountPriceForMultiple() {
+        int loopCount = 10;
+        for (int multipleUnit = 1; multipleUnit <= loopCount; multipleUnit++) {
+            Money totalOrderPrice = new Money( // 12만원 * i
+                    ConstantMoney.STANDARD_FOR_PRESENTATION_EVENT.getAmount() * multipleUnit);
+            Order order = new Order(tempDay, tempMenuCounts, totalOrderPrice);
+            assertThat(presentationCalculator.discountPrice(order)).isEqualTo(
+                    ConstantMoney.SIGN_INVERTER.getAmount() * Menu.CHAMPAGNE.getAmount()
+                            * multipleUnit
+            );
+        }
     }
 }
