dc2d1cfe8c60ba1699f9d8540b85ca28e0d45f17
kyeoungchan <wkc1002@naver.com>
2023-11-14 14:44:23 +0900

    feat: WeekendCalculator discountPrice()

--- src/main/java/christmas/domain/WeekendCalculator.java
+++ src/main/java/christmas/domain/WeekendCalculator.java
@@ -1,8 +1,12 @@
 package christmas.domain;
 
+import christmas.consts.ConstantMoney;
+import christmas.consts.MenuKind;
 import christmas.vo.Day;
 import christmas.vo.Money;
 import christmas.vo.Order;
+
+import java.util.Map;
 
 public class WeekendCalculator implements EventCalculatorAdapter {
     @Override
@@ -12,6 +16,16 @@ public class WeekendCalculator implements EventCalculatorAdapter {
 
     @Override
     public int discountPrice(Order order) {
-        return 0;
+        int mainMenuCount = countMainMenu(order);
+        return mainMenuCount * ConstantMoney.INCREASE_UNIT_FOR_WEEKEND_EVENT.getAmount()
+                * ConstantMoney.SIGN_INVERTER.getAmount();
+    }
+
+    private int countMainMenu(Order order) {
+        return order.menuCount().entrySet()
+                .stream()
+                .filter(entry -> MenuKind.MAIN.isKindOf(entry.getKey()))
+                .mapToInt(Map.Entry::getValue)
+                .sum();
     }
 }
