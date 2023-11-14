30bb3fb4ad72c5f5b000505b7336e2ac7a9225ba
kyeoungchan <wkc1002@naver.com>
2023-11-14 23:19:52 +0900

    fix: OrderCalculatorTest calculateTotalPriceAndCanApplyToEvents() 지역 변수 사용

--- src/test/java/christmas/domain/OrderCalculatorTest.java
+++ src/test/java/christmas/domain/OrderCalculatorTest.java
@@ -16,12 +16,14 @@ class OrderCalculatorTest {
     @Test
     @DisplayName("메뉴 가격들을 다 더한 가격이 10,000원 이상이면 이벤트에 참여 가능하다.")
     void calculateTotalPriceAndCanApplyToEvents() {
-        Menu orderMenu = Menu.T_BONE_STEAK;
-        int totalAmount = orderMenu.getAmount();
-        menuCounts.put(orderMenu, 1);
+        Menu menu1 = Menu.T_BONE_STEAK;
+        menuCounts.put(menu1, 1);
+        Menu menu2 = Menu.RED_WINE;
+        int menu2Count = 2;
+        menuCounts.put(menu2, menu2Count);
+        int totalAmount = menu1.getAmount() + menu2.getAmount() * menu2Count;
 
         OrderBeforeEvents result = orderCalculator.calculateTotalPriceIfCanApplyToEvents(menuCounts);
-
         assertThat(result.totalOrderPrice().getAmount()).isEqualTo(totalAmount);
         assertThat(result.canApplyToEvents()).isTrue();
     }
