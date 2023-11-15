71113d6b8893821e805d33f05d19706837579e55
kyeoungchan <wkc1002@naver.com>
2023-11-15 17:38:05 +0900

    feat: Validate OrderMenus

--- src/test/java/christmas/domain/MenuOrderAssemblerTest.java
+++ src/test/java/christmas/domain/MenuOrderAssemblerTest.java
@@ -5,6 +5,7 @@ import christmas.consts.Menu;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.EnumMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
@@ -17,13 +18,31 @@ class MenuOrderAssemblerTest {
     @Test
     @DisplayName("입력받은 메뉴 이름과 개수에 대한 정보가 문제 없으면 EnumMap 타입으로 전달한다.")
     void generateMenuCounts() {
-        List<String> menuNames = List.of("양송이수프", "타파스");
-        List<Integer> counts = List.of(1, 2);
-        assertThatNoException().isThrownBy(() -> menuOrderAssembler.generateMenuCounts(menuNames, counts));
-        assertThat(menuOrderAssembler.generateMenuCounts(menuNames, counts).keySet())
-                .containsExactly(Menu.BUTTON_MUSHROOM_SOUP, Menu.TAPAS);
-        assertThat(menuOrderAssembler.generateMenuCounts(menuNames, counts).values())
-                .containsExactly(1, 2);
+        List<String> menuNames = List.of("해산물파스타", "레드와인", "초코케이크");
+        List<Integer> counts = List.of(1, 3, 2);
+        EnumMap<Menu, Integer> result = menuOrderAssembler.generateMenuCounts(menuNames, counts);
+        assertThat(result.keySet())
+                .containsExactlyInAnyOrder(Menu.SEAFOOD_PASTA, Menu.RED_WINE, Menu.CHOCOLATE_CAKE);
+        assertThat(result.values())
+                .containsExactlyInAnyOrder(1, 2, 3);
+        assertThat(result.get(Menu.SEAFOOD_PASTA)).isEqualTo(1);
+        assertThat(result.get(Menu.RED_WINE)).isEqualTo(3);
+        assertThat(result.get(Menu.CHOCOLATE_CAKE)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("입력받은 메뉴 이름과 개수에 대한 정보가 문제 없으면 EnumMap 타입으로 전달한다.")
+    void generateMenuCounts2() {
+        List<String> menuNames = List.of("제로콜라", "티본스테이크", "초코케이크");
+        List<Integer> counts = List.of(1, 3, 2);
+        EnumMap<Menu, Integer> result = menuOrderAssembler.generateMenuCounts(menuNames, counts);
+        assertThat(result.keySet())
+                .containsExactlyInAnyOrder(Menu.T_BONE_STEAK, Menu.COKE_ZERO, Menu.CHOCOLATE_CAKE);
+        assertThat(result.values())
+                .containsExactlyInAnyOrder(1, 2, 3);
+        assertThat(result.get(Menu.COKE_ZERO)).isEqualTo(1);
+        assertThat(result.get(Menu.T_BONE_STEAK)).isEqualTo(3);
+        assertThat(result.get(Menu.CHOCOLATE_CAKE)).isEqualTo(2);
     }
 
     @Test
@@ -62,13 +81,10 @@ class MenuOrderAssemblerTest {
     @Test
     @DisplayName("당연히 메뉴 갯수는 중복돼도 상관없다.")
     void duplicatedCounts() {
-        List<String> menuNames = List.of("양송이수프", "타파스");
-        List<Integer> counts = List.of(1, 1);
+        List<String> menuNames = List.of("해산물파스타", "레드와인", "초코케이크");
+        List<Integer> counts = List.of(1, 1, 1);
         assertThatNoException().isThrownBy(() -> menuOrderAssembler.generateMenuCounts(menuNames, counts));
         assertThat(menuOrderAssembler.generateMenuCounts(menuNames, counts).keySet())
-                .containsExactly(Menu.BUTTON_MUSHROOM_SOUP, Menu.TAPAS);
-        assertThat(menuOrderAssembler.generateMenuCounts(menuNames, counts).values())
-                .containsExactly(1, 1);
+                .containsExactlyInAnyOrder(Menu.SEAFOOD_PASTA, Menu.RED_WINE, Menu.CHOCOLATE_CAKE);
     }
-
 }
--- src/test/java/christmas/util/InputValidatorTest.java
+++ src/test/java/christmas/util/InputValidatorTest.java
@@ -1,12 +1,16 @@
 package christmas.util;
 
 import christmas.consts.ErrorMessage;
+import christmas.consts.Menu;
 import christmas.domain.MenuOrderAssembler;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.EnumMap;
+
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class InputValidatorTest {
     private final InputValidator inputValidator = new InputValidator(new MenuOrderAssembler());
@@ -66,9 +70,19 @@ class InputValidatorTest {
     }
 
     @Test
+    @DisplayName("형식에 맞춰 입력하면 메뉴와 갯수를 EnumMap에 넣어서 반환")
+    void validateMenus() {
+        String validatedInput = "해산물파스타-2,레드와인-1,초코케이크-1";
+        EnumMap<Menu, Integer> result = inputValidator.validateMenus(validatedInput);
+        assertThat(result.get(Menu.SEAFOOD_PASTA)).isEqualTo(2);
+        assertThat(result.get(Menu.RED_WINE)).isEqualTo(1);
+        assertThat(result.get(Menu.CHOCOLATE_CAKE)).isEqualTo(1);
+    }
+
+    @Test
     @DisplayName("쉼표 사이에 공백이면 검증 실패")
     void inputBlankBetweenComma() {
-        String unvalidatedInput = "가,,다";
+        String unvalidatedInput = "해산물파스타-2,,레드와인-1";
         assertThatThrownBy(() -> inputValidator.validateMenus(unvalidatedInput))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> inputValidator.parseToValidatedInt(unvalidatedInput)).hasMessageContaining(
@@ -81,5 +95,49 @@ class InputValidatorTest {
     void inputBlankWithValidatedData() {
         String validatedInput = "  해산물파스타-2   ,        레드와인-1    ,     초코케이크-1  ";
         assertThatNoException().isThrownBy(() -> inputValidator.validateMenus(validatedInput));
+    }
+
+    @Test
+    @DisplayName("-바로 문자열을 나눴을 때 갯수가 0개면 검증 실패")
+    void inputZeroSepartedByBar() {
+        String unvalidatedInput = "해산물파스타-2,-,초코케이크-1";
+        assertThatThrownBy(() -> inputValidator.validateMenus(unvalidatedInput))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> inputValidator.parseToValidatedInt(unvalidatedInput)).hasMessageContaining(
+                ErrorMessage.ERROR_PREFIX.getMessage(), ErrorMessage.UNVALIDATED_ORDER, ErrorMessage.ERROR_POSTFIX
+        );
+    }
+
+    @Test
+    @DisplayName("-바로 문자열을 나눴을 때 갯수가 1개면 검증 실패")
+    void inputOneSepartedByBar() {
+        String unvalidatedInput = "해산물파스타-2,레드와인-1,초코케이크-";
+        assertThatThrownBy(() -> inputValidator.validateMenus(unvalidatedInput))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> inputValidator.parseToValidatedInt(unvalidatedInput)).hasMessageContaining(
+                ErrorMessage.ERROR_PREFIX.getMessage(), ErrorMessage.UNVALIDATED_ORDER, ErrorMessage.ERROR_POSTFIX
+        );
+    }
+
+    @Test
+    @DisplayName("-바로 문자열을 나눴을 때 갯수가 3개면 검증 실패")
+    void inputThreeSepartedByBar() {
+        String unvalidatedInput = "해산물파스타-2,레드와인-1-아이스크림,초코케이크-1";
+        assertThatThrownBy(() -> inputValidator.validateMenus(unvalidatedInput))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> inputValidator.parseToValidatedInt(unvalidatedInput)).hasMessageContaining(
+                ErrorMessage.ERROR_PREFIX.getMessage(), ErrorMessage.UNVALIDATED_ORDER, ErrorMessage.ERROR_POSTFIX
+        );
+    }
+
+    @Test
+    @DisplayName("개수 자리에 숫자가 아닌 것 입력하면 검증 실패")
+    void inputNotNumberCount() {
+        String unvalidatedInput = "해산물파스타-2,레드와인-아이스크림,초코케이크-2";
+        assertThatThrownBy(() -> inputValidator.validateMenus(unvalidatedInput))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> inputValidator.parseToValidatedInt(unvalidatedInput)).hasMessageContaining(
+                ErrorMessage.ERROR_PREFIX.getMessage(), ErrorMessage.UNVALIDATED_ORDER, ErrorMessage.ERROR_POSTFIX
+        );
     }
 }
